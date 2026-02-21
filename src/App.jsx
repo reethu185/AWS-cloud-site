1067df32f2dcb74a8220984b0a9c8d4ea978a94d
reethu185 <reethu132006@gmail.com>
2026-02-21 19:21:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,8 @@ export default function App() {
       {/* Recruitment Popup */}
       <RecruitmentPopup />
 
-      <div className="min-h-screen bg-navy text-off flex items-center justify-center">
-        <div className="w-full h-full min-h-screen max-w-[1920px] border border-off/20 rounded-2xl mx-auto">
+      <div className="min-h-screen bg-navy text-off">
+        <div className="w-full min-h-screen max-w-[1920px] border border-off/20 rounded-2xl mx-auto">
           
           <Navbar />
 
